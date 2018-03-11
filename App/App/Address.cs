83021f1b3cec430f04b8556a330b795643391320
jordanhewko <jordanhewko@DESKTOP-NMH7678.localdomain>
2018-03-11 13:38:30 -0700

    Create Address, ContactInformation, UserName, InsertionParameters class, also inserting customers

---
+++ App/App/Address.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Address
+    {
+        private String suiteNumber;
+        private String streetNumber;
+        private String houseNumber;
+        private String city;
+        private String province;
+        private String postalCode;
+
+        public Address(String suite, String street, String house,String town, String prov, String postal)
+        {
+            suiteNumber = suite;
+            streetNumber = street;
+            houseNumber = house;
+            city = town;
+            province = prov;
+            postalCode = postal;
+        }
+
+        public String GetSuiteNumber()
+        {
+            return suiteNumber;
+        }
+        public String GetStreetNumber()
+        {
+            return streetNumber;
+        }
+        public String GetHouseNumber()
+        {
+            return houseNumber;
+        }
+        public String GetCity()
+        {
+            return city;
+        }
+        public String GetProvince()
+        {
+            return province;
+        }
+        public String GetPostal()
+        {
+            return postalCode;
+        }
+
+    }
+}
