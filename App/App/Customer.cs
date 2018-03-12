8a7eb9323721fbb64aba88699918de486080030a
Aidan Bush <aidandbush@gmail.com>
2018-03-11 21:08:13 -0600

    Add template Person and Customer Classes

---
+++ App/App/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace App
+{
+    public class Customer : Person
+    {
+        enum accountType {Limited, Bronze, Silver, Gold};
+        private accountType type;
+        private DateTime creationDate;
+        private string creditCard;
+
+        public Customer(string newEmail, accountType newType)
+        {
+            SetType(newType);
+            SetEmail(email);
+        }
+
+        public string GetcreditCard()
+        {
+            return creditCard;
+        }
+
+        public void SetcreditCard(string value)
+        {
+            creditCard = value;
+        }
+
+        private accountType GetType()
+        {
+            return type;
+        }
+
+        private void SetType(accountType value)
+        {
+            type = value;
+        }
+    }
+}
---
+++ App/App/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace App
+{
+    class Person
+    {
+        private int cid;
+        // name
+        // address
+        private string email;
+        private string phoneNumber;
+
+        public int GetCid()
+        {
+            return cid;
+        }
+
+        public void SetCid(int value)
+        {
+            cid = value;
+        }
+
+        public string GetEmail()
+        {
+            return email;
+        }
+
+        public void SetEmail(string value)
+        {
+            email = value;
+        }
+
+        public string GetPhoneNumber()
+        {
+            return phoneNumber;
+        }
+
+        public void SetPhoneNumber(string value)
+        {
+            phoneNumber = value;
+        }
+    }
+
+}
