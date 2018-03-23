edccb6a9458bd66818ab84ce2530c4c78ad011b8
EvanGee <evangiese77@gmail.com>
2018-03-22 19:07:53 -0600

    Add Employee Add Edit form

---
+++ App/App/EmployeeAddEdit.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace App
+{
+    partial class EmployeeAddEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CityBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.PhoneBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.PostalBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProvinceBox = new System.Windows.Forms.TextBox();
+            this.StreetBox = new System.Windows.Forms.TextBox();
+            this.HouseBox = new System.Windows.Forms.TextBox();
+            this.SuiteBox = new System.Windows.Forms.TextBox();
+            this.LastNameBox = new System.Windows.Forms.TextBox();
+            this.FirstNameBox = new System.Windows.Forms.TextBox();
+            this.AddUserButton = new System.Windows.Forms.Button();
+            this.SIN = new System.Windows.Forms.Label();
+            this.SINBox = new System.Windows.Forms.TextBox();
+            this.Possition = new System.Windows.Forms.Label();
+            this.PossitionBox = new System.Windows.Forms.TextBox();
+            this.WageLabel = new System.Windows.Forms.Label();
+            this.WageBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CityBox
+            // 
+            this.CityBox.Location = new System.Drawing.Point(103, 349);
+            this.CityBox.Margin = new System.Windows.Forms.Padding(2);
+            this.CityBox.Name = "CityBox";
+            this.CityBox.Size = new System.Drawing.Size(103, 22);
+            this.CityBox.TabIndex = 30;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(99, 323);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 17);
+            this.label11.TabIndex = 44;
+            this.label11.Text = "City:";
+            // 
+            // PhoneBox
+            // 
+            this.PhoneBox.Location = new System.Drawing.Point(20, 403);
+            this.PhoneBox.Margin = new System.Windows.Forms.Padding(2);
+            this.PhoneBox.Name = "PhoneBox";
+            this.PhoneBox.Size = new System.Drawing.Size(191, 22);
+            this.PhoneBox.TabIndex = 36;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(21, 385);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(107, 17);
+            this.label10.TabIndex = 43;
+            this.label10.Text = "Phone Number:";
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(20, 439);
+            this.CancelButton.Margin = new System.Windows.Forms.Padding(2);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(93, 35);
+            this.CancelButton.TabIndex = 42;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(205, 323);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 17);
+            this.label8.TabIndex = 40;
+            this.label8.Text = "Postal Code:";
+            // 
+            // PostalBox
+            // 
+            this.PostalBox.Location = new System.Drawing.Point(208, 349);
+            this.PostalBox.Margin = new System.Windows.Forms.Padding(2);
+            this.PostalBox.Name = "PostalBox";
+            this.PostalBox.Size = new System.Drawing.Size(102, 22);
+            this.PostalBox.TabIndex = 32;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 323);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 17);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "Province: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(205, 269);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 17);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "Street Number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(99, 269);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 17);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "House Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 269);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "Suite:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "Last Name:*";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "First Name:*";
+            // 
+            // ProvinceBox
+            // 
+            this.ProvinceBox.Location = new System.Drawing.Point(20, 349);
+            this.ProvinceBox.Margin = new System.Windows.Forms.Padding(2);
+            this.ProvinceBox.Name = "ProvinceBox";
+            this.ProvinceBox.Size = new System.Drawing.Size(68, 22);
+            this.ProvinceBox.TabIndex = 29;
+            // 
+            // StreetBox
+            // 
+            this.StreetBox.Location = new System.Drawing.Point(208, 293);
+            this.StreetBox.Margin = new System.Windows.Forms.Padding(2);
+            this.StreetBox.Name = "StreetBox";
+            this.StreetBox.Size = new System.Drawing.Size(102, 22);
+            this.StreetBox.TabIndex = 28;
+            // 
+            // HouseBox
+            // 
+            this.HouseBox.Location = new System.Drawing.Point(103, 293);
+            this.HouseBox.Margin = new System.Windows.Forms.Padding(2);
+            this.HouseBox.Name = "HouseBox";
+            this.HouseBox.Size = new System.Drawing.Size(103, 22);
+            this.HouseBox.TabIndex = 27;
+            // 
+            // SuiteBox
+            // 
+            this.SuiteBox.Location = new System.Drawing.Point(20, 293);
+            this.SuiteBox.Margin = new System.Windows.Forms.Padding(2);
+            this.SuiteBox.Name = "SuiteBox";
+            this.SuiteBox.Size = new System.Drawing.Size(68, 22);
+            this.SuiteBox.TabIndex = 26;
+            // 
+            // LastNameBox
+            // 
+            this.LastNameBox.Location = new System.Drawing.Point(20, 91);
+            this.LastNameBox.Margin = new System.Windows.Forms.Padding(2);
+            this.LastNameBox.Name = "LastNameBox";
+            this.LastNameBox.Size = new System.Drawing.Size(186, 22);
+            this.LastNameBox.TabIndex = 25;
+            // 
+            // FirstNameBox
+            // 
+            this.FirstNameBox.Location = new System.Drawing.Point(21, 45);
+            this.FirstNameBox.Margin = new System.Windows.Forms.Padding(2);
+            this.FirstNameBox.Name = "FirstNameBox";
+            this.FirstNameBox.Size = new System.Drawing.Size(186, 22);
+            this.FirstNameBox.TabIndex = 24;
+            // 
+            // AddUserButton
+            // 
+            this.AddUserButton.Location = new System.Drawing.Point(117, 438);
+            this.AddUserButton.Margin = new System.Windows.Forms.Padding(2);
+            this.AddUserButton.Name = "AddUserButton";
+            this.AddUserButton.Size = new System.Drawing.Size(92, 36);
+            this.AddUserButton.TabIndex = 23;
+            this.AddUserButton.Text = "Add User";
+            this.AddUserButton.UseVisualStyleBackColor = true;
+            // 
+            // SIN
+            // 
+            this.SIN.AutoSize = true;
+            this.SIN.Location = new System.Drawing.Point(21, 160);
+            this.SIN.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.SIN.Name = "SIN";
+            this.SIN.Size = new System.Drawing.Size(155, 17);
+            this.SIN.TabIndex = 46;
+            this.SIN.Text = "Social Security Number";
+            // 
+            // SINBox
+            // 
+            this.SINBox.Location = new System.Drawing.Point(20, 181);
+            this.SINBox.Margin = new System.Windows.Forms.Padding(2);
+            this.SINBox.Name = "SINBox";
+            this.SINBox.Size = new System.Drawing.Size(186, 22);
+            this.SINBox.TabIndex = 45;
+            // 
+            // Possition
+            // 
+            this.Possition.AutoSize = true;
+            this.Possition.Location = new System.Drawing.Point(21, 115);
+            this.Possition.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Possition.Name = "Possition";
+            this.Possition.Size = new System.Drawing.Size(65, 17);
+            this.Possition.TabIndex = 48;
+            this.Possition.Text = "Possition";
+            // 
+            // PossitionBox
+            // 
+            this.PossitionBox.Location = new System.Drawing.Point(20, 136);
+            this.PossitionBox.Margin = new System.Windows.Forms.Padding(2);
+            this.PossitionBox.Name = "PossitionBox";
+            this.PossitionBox.Size = new System.Drawing.Size(186, 22);
+            this.PossitionBox.TabIndex = 47;
+            // 
+            // WageLabel
+            // 
+            this.WageLabel.AutoSize = true;
+            this.WageLabel.Location = new System.Drawing.Point(21, 215);
+            this.WageLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.WageLabel.Name = "WageLabel";
+            this.WageLabel.Size = new System.Drawing.Size(45, 17);
+            this.WageLabel.TabIndex = 52;
+            this.WageLabel.Text = "Wage";
+            // 
+            // WageBox
+            // 
+            this.WageBox.Location = new System.Drawing.Point(20, 236);
+            this.WageBox.Margin = new System.Windows.Forms.Padding(2);
+            this.WageBox.Name = "WageBox";
+            this.WageBox.Size = new System.Drawing.Size(116, 22);
+            this.WageBox.TabIndex = 51;
+            // 
+            // EmployeeAddEdit
+            // 
+            this.ClientSize = new System.Drawing.Size(328, 489);
+            this.Controls.Add(this.WageLabel);
+            this.Controls.Add(this.WageBox);
+            this.Controls.Add(this.Possition);
+            this.Controls.Add(this.PossitionBox);
+            this.Controls.Add(this.SIN);
+            this.Controls.Add(this.SINBox);
+            this.Controls.Add(this.CityBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.PhoneBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.PostalBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ProvinceBox);
+            this.Controls.Add(this.StreetBox);
+            this.Controls.Add(this.HouseBox);
+            this.Controls.Add(this.SuiteBox);
+            this.Controls.Add(this.LastNameBox);
+            this.Controls.Add(this.FirstNameBox);
+            this.Controls.Add(this.AddUserButton);
+            this.Name = "EmployeeAddEdit";
+            this.Load += new System.EventHandler(this.EmployeeAddEdit_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CityBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox PhoneBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox PostalBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ProvinceBox;
+        private System.Windows.Forms.TextBox StreetBox;
+        private System.Windows.Forms.TextBox HouseBox;
+        private System.Windows.Forms.TextBox SuiteBox;
+        private System.Windows.Forms.TextBox LastNameBox;
+        private System.Windows.Forms.TextBox FirstNameBox;
+        private System.Windows.Forms.Button AddUserButton;
+        private System.Windows.Forms.Label SIN;
+        private System.Windows.Forms.TextBox SINBox;
+        private System.Windows.Forms.Label Possition;
+        private System.Windows.Forms.TextBox PossitionBox;
+        private System.Windows.Forms.Label WageLabel;
+        private System.Windows.Forms.TextBox WageBox;
+    }
+}
---
+++ App/App/EmployeeAddEdit.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class EmployeeAddEdit : Form
+    {
+        Employee employee;
+        ManagerForm parent;
+        public enum FormType { add, edit };
+
+        public EmployeeAddEdit(Employee selectedEmployee, ManagerForm form)
+        {
+            
+            parent = form;
+            InitializeComponent();
+            if (selectedEmployee == null)
+            {
+                return;
+            }
+            employee = selectedEmployee;
+            FillTextBoxes();
+
+
+        }
+        
+        private void FillTextBoxes()
+        {
+            FirstNameBox.Text = employee.Name.FirstName;
+            LastNameBox.Text = employee.Name.LastName;
+            SuiteBox.Text = employee.Address.SuiteNumber;
+            StreetBox.Text = employee.Address.StreetNumber;
+            ProvinceBox.Text = employee.Address.Province;
+            PostalBox.Text = employee.Address.PostalCode;
+            CityBox.Text = employee.Address.City;
+            HouseBox.Text = employee.Address.HouseNumber;
+            PhoneBox.Text = employee.ContactInformation.PhoneNumber;
+            SINBox.Text = employee.SIN;
+            WageBox.Text = employee.Wage.ToString();
+            PossitionBox.Text = employee.EmployeePosition.ToString();
+ 
+        }
+        private void EditUser()
+        {
+            Employee updatedEmployee = employee;
+            if (employee.Name.FirstName != FirstNameBox.Text)
+            {
+                updatedEmployee.Name.FirstName = FirstNameBox.Text;
+            }
+            if (employee.Name.LastName != LastNameBox.Text)
+            {
+                updatedEmployee.Name.LastName = LastNameBox.Text;
+            }
+            if (employee.Address.City != CityBox.Text)
+            {
+                updatedEmployee.Address.City = CityBox.Text;
+            }
+            if (employee.Address.HouseNumber != HouseBox.Text)
+            {
+                updatedEmployee.Address.HouseNumber = HouseBox.Text;
+            }
+            if (employee.Address.PostalCode != PostalBox.Text)
+            {
+                updatedEmployee.Address.PostalCode = PostalBox.Text;
+            }
+            if (employee.Address.Province != ProvinceBox.Text)
+            {
+                updatedEmployee.Address.Province = ProvinceBox.Text;
+            }
+            if (employee.Address.StreetNumber != StreetBox.Text)
+            {
+                updatedEmployee.Address.StreetNumber = StreetBox.Text;
+            }
+            if (employee.Address.SuiteNumber != SuiteBox.Text)
+            {
+                updatedEmployee.Address.SuiteNumber = SuiteBox.Text;
+            }
+            if (employee.ContactInformation.PhoneNumber != PhoneBox.Text)
+            {
+                updatedEmployee.ContactInformation.PhoneNumber = PhoneBox.Text;
+            }
+            if (SINBox.Text != employee.SIN)
+            {
+                updatedEmployee.SIN = SINBox.Text;
+            }
+            if (WageBox.Text != employee.Wage.ToString())
+            {
+                updatedEmployee.Wage = float.Parse(WageBox.Text, CultureInfo.InvariantCulture.NumberFormat);
+            }
+            if (PossitionBox.Text == Employee.Position.Employee.ToString())
+            {
+                updatedEmployee.EmployeePosition = Employee.Position.Employee;
+            }
+            else if(PossitionBox.Text == Employee.Position.Manager.ToString())
+            {
+                updatedEmployee.EmployeePosition = Employee.Position.Manager;
+            }
+
+            DBEnvironment.Edit(updatedEmployee);
+            MessageBox.Show("Employee edit complete!");
+        }
+  
+        private void EditUserButton_Click(object sender, EventArgs e)
+        {
+            if ((MessageBox.Show("Edit Employee with current information?", "Confirm",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button1) == System.Windows.Forms.DialogResult.Yes))
+            {
+                EditUser();
+                parent.FillTable();
+                this.Close();
+            }
+
+
+        }
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EmployeeAddEdit_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
